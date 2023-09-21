a7a48db39e8aa1775f6c56406ead52cd9ace3d0a
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-09-21 21:22:50 +0700

    ADD: getUserByLoginID

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,12 +3,14 @@ import express from "express";
 import {
   getUser,
   createUser,
-  updateUsername
+  getUserByLoginID,
+  updateUsername,
 } from "../components/user.js";
 
 const router = express.Router();
 
 router.get("/", getUser);
+router.post("/", getUserByLoginID);
 router.post("/create", createUser);
 router.post("/update", updateUsername);
 
