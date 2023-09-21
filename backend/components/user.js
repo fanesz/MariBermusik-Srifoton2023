c64d5ee63134957396222a05617d27577487ee1f
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-09-21 21:45:33 +0700

    ADD: getUserByLoginID

--- backend/components/user.js
+++ backend/components/user.js
@@ -2,11 +2,23 @@ import { QuickDB } from "quick.db";
 import { generateUUID } from "../utils/utils.js";
 const db = new QuickDB();
 const db_user = db.table("user");
+const db_loggedUser = db.table("loggedUser");
 
 export const getUser = async (req, res) => {
   try {
     const users = await db_user.get() || {};
     res.json({ status: true, data: users });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: false });
+  }
+}
+
+export const getUserByLoginID = async (req, res) => {
+  try { // body: { loginID }
+    const loggedUser = await db_loggedUser.get(req.body.loginID);
+    const user = await db_user.get(loggedUser.id);
+    res.json({ status: true, data: user });
   } catch (error) {
     console.log(error);
     res.json({ status: false });
@@ -21,7 +33,12 @@ export const createUser = async (req, res) => {
       res.json({ status: false });
       return;
     }
-    await db_user.set(generateUUID(), userInput);
+    await db_user.set(generateUUID(), {
+      email: userInput.email,
+      password: userInput.password,
+      username: userInput.username,
+      akses: "user"
+    });
     res.json({ status: true })
   } catch (error) {
     console.log(error);
@@ -29,6 +46,7 @@ export const createUser = async (req, res) => {
   }
 }
 
+// todo: check if username avaiable
 export const updateUsername = async (req, res) => {
   try { // body: { email, username }
     const userInput = req.body;
