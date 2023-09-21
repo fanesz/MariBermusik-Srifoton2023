ec473fb244d6092445f20f64437fb19cf65d8bc5
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-09-21 20:12:24 +0700

    FIX: login components

--- backend/components/login.js
+++ backend/components/login.js
@@ -1,13 +1,21 @@
 import { QuickDB } from "quick.db";
 const db = new QuickDB();
 const db_loggedUser = db.table("loggedUser");
+const db_user = db.table("user");
 import { generateRandomString } from "../utils/utils.js";
 
 export const setLogin = async (req, res) => {
-  try { // body: { email }
+  try { // body: { email, password }
     const userInput = req.body;
     const loginID = generateRandomString(80);
-    await db_loggedUser.set(loginID, userInput);
+    const userData = (await db_user.all()).filter(u => u.value.email === userInput.email)[0];
+    if (!userData) {
+      return res.send({ status: false, message: "Wrong Email!" });
+    }
+    if (userData.value.password !== userInput.password) {
+      return res.send({ status: false, message: "Wrong Password!" });
+    }
+    await db_loggedUser.set(loginID, { id: userData.id, email: userInput.email });
     res.send({ status: true, loginID: loginID });
   } catch (error) {
     console.log(error);
