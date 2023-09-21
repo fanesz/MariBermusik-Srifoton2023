9f502267e61d8d42d9829e1bf6087c368709d16b
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-09-21 13:22:58 +0700

    ADD: logout route

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -2,12 +2,14 @@ import express from "express";
 
 import {
   setLogin,
+  setLogout,
   isLogin
 } from "../components/login.js";
 
 const router = express.Router();
 
 router.post("/", setLogin);
+router.post("/out", setLogout);
 router.post("/isLogin", isLogin);
 
 export default router;
