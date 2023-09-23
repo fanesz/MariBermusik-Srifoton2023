2fcf3b3b694708fc57cd3ac932864114664368f0
Rendy Pratama <126235207+Rendy752@users.noreply.github.com>
2023-09-23 08:37:59 +0700

    STYLE: navbar

--- frontend/src/components/_shared/Navbar.tsx
+++ frontend/src/components/_shared/Navbar.tsx
@@ -5,37 +5,42 @@ const Navbar = () => {
 
   return (
     <nav>
-      <ul className="xl:flex xl:justify-between items-center text-center">
+      <div className="" id="navbar-default">
+      <ul className="xl:flex xl:justify-around items-center text-center">
         <li className="flex justify-center text-center">
           <a href="#"><img src={logo} alt="MariBermusik Logo" id="logo"/></a>
           <span className="text-3xl text-emerald-950 hover:text-black">Mari</span><span className="text-3xl text-emerald-400 hover:text-emerald-500">Bermusik</span>
         </li>
-        <li>
-              <form action="">
-                <input className="bg-slate-100 text-black border-gray-300 rounded h-10 w-72 p-3" type="search" placeholder="Search musical instrumental ..." />
-                <button type="submit" className="bg-blue-700 rounded transition duration-300 p-2 hover:bg-blue-900">Search</button>
-              </form>
-            </li>
+        <li className="cursor-default">
+          <form action="">
+            <input className="bg-slate-100 text-black border-gray-300 rounded h-10 w-64 p-3" type="search" placeholder="Search musical instrumental ..." />
+            <button type="submit" className="bg-blue-700 rounded transition duration-300 p-2 hover:bg-blue-900">Search</button>
+          </form>
+        </li>
         <div>
-          <ul className="md:flex md:justify-between items-center text-center">
-            
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Category List</a></li>
-            <li><a href="#">Forum</a></li>
-            <li><a href="#">About</a></li>
-            <li className="flex justify-center">
-              <img id="profile" data-dropdown-toggle="profileMenu" data-dropdown-trigger="hover" src={profile}/>
-              <div id="profileMenu" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                <ul aria-labelledby="profile">
-                  <li><a href="#">Profil</a></li>
-                  <li><a href="#">Pengaturan</a></li>
-                  <li><a href="#">Sign out</a></li>
-                </ul>
-              </div>
-            </li>
+          <ul className="sm:flex sm:justify-around items-center text-center">
+            <a href="#"><li>Home</li></a>
+            <a href="#"><li>Category List</li></a>
+            <a href="#"><li>Forum</li></a>
+            <a href="#"><li>About</li></a>
           </ul>
         </div>
+        <li className="flex justify-center xl:order-last" data-dropdown-toggle="profileMenu" data-dropdown-trigger="hover">
+              <img id="profile" src={profile}/>
+              <div id="profileMenu" className="hidden rounded-lg">
+                <ul aria-labelledby="profile">
+                <div className="p-3 bg-gradient-to-br  via-amber-100 from-orange-200 rounded cursor-default">
+                  <span className="block text-sm text-gray-900 dark:text-white">Hoho</span>
+                  <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">hoho@gmail.com</span>
+                </div>
+                  <a href="#"><li>Profile</li></a>
+                  <a href="#"><li>Setting</li></a>
+                  <a href="#"><li>Sign out</li></a>
+                </ul>
+              </div>
+        </li>
       </ul>
+      </div>
     </nav>
   )
 }
