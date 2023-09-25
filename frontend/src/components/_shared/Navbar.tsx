387f787c964d1370d091755c5237ef506fd3d1a3
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-09-25 13:50:47 +0700

    WIP: headlessui implementation 2

--- frontend/src/components/_shared/Navbar.tsx
+++ frontend/src/components/_shared/Navbar.tsx
@@ -13,25 +13,25 @@ const Navbar = () => {
             <img src={logo} alt="MariBermusik Logo" className="md:h-12 h-6" />
           </a>
         </div>
-        <div className="hidden xl:block text-3xl mt-auto mb-auto ms-4 font-bold">
-          <span className="text-emerald-950 hover:text-black">Mari</span>
-          <span className="text-emerald-400 hover:text-emerald-500">Bermusik</span>
-          <span className="text-emerald-950 hover:text-black">.</span>
+        <div className="hidden xl:block text-3xl mt-auto mb-auto ms-4 font-bold cursor-pointer">
+          <span className="text-emerald-950 hover:text-black duration-200">Mari</span>
+          <span className="text-emerald-400 hover:text-emerald-500 duration-200">Bermusik</span>
+          <span className="text-emerald-950 hover:text-black duration-200">.</span>
         </div>
       </div>
 
       {/* Menu Desktop */}
       <div className="hidden md:flex me-7 gap-10">
-        <div className="flex mt-auto mb-auto gap-8 border font-medium">
-          <a href="#" className="border">Home</a>
-          <a href="#" className="border">Category List</a>
-          <a href="#" className="border">Forum</a>
-          <a href="#" className="border">About</a>
+        <div className="flex mt-auto mb-auto gap-2 text-lg font-medium">
+          <a href="#" className="navbar_menu_items">Home</a>
+          <a href="#" className="navbar_menu_items">Category List</a>
+          <a href="#" className="navbar_menu_items">Forum</a>
+          <a href="#" className="navbar_menu_items">About</a>
         </div>
-        <div className="border flex">
+        <div className="flex">
           <Menu as="div" className="relative">
             <Menu.Button as="button" className="">
-              <img id="profile" src={profile} className="h-12" />
+              <img className="h-12 hover:scale-105 transition-all" id="profile" src={profile} />
             </Menu.Button>
             <Transition
               enter="transition-transform origin-top duration-400"
@@ -71,7 +71,7 @@ const Navbar = () => {
             leaveFrom="scale-y-100 opacity-100"
             leaveTo="scale-y-0 opacity-100"
           >
-            <Menu.Items className="bg-gray-100 bg-opacity-90 border border-gray-300 rounded-md py-1">
+            <Menu.Items className="absolute bg-gray-100 right-0 bg-opacity-90 border border-gray-300 rounded-md py-1">
               <Menu.Item as="div" className="border-t border-opacity-50 px-4 py-1 truncate">
                 Fanes Pratama
               </Menu.Item>
