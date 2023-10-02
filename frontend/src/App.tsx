f075106b6b47c8ccb62bc4e8051ec43885bd8e84
Rendy Pratama <126235207+Rendy752@users.noreply.github.com>
2023-10-02 21:11:05 +0700

    WIP: forum

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/main/Home"
 import Forum from "./pages/main/Forum"
+import PostForum from "./pages/main/PostForum"
 import Navbar from "./components/_shared/Navbar"
 import Footer from "./components/_shared/Footer"
 import ResetPassword from "./pages/other/ResetPassword"
@@ -26,6 +27,7 @@ function App() {
           <Route path='/materi/:alatmusik/:id' element={<Materi />} />
 
           <Route path='/forum' element={<Forum />} />
+          <Route path='/postForum' element={<PostForum />} />
 
           <Route path='/resetpassword/:id' element={<ResetPassword />} />
 
--- frontend/src/pages/main/Forum.tsx
+++ frontend/src/pages/main/Forum.tsx
@@ -1,39 +1,92 @@
+import { useEffect, useState } from "react";
+import { getPost, getPostByOwner, getUser } from "../../api/services";
+import { Link } from 'react-router-dom';
+
 const Forum = () => {
+  const [post,setPost]=useState<any[]>([])
+  const [user,setUser]=useState<any[]>([])
+  const [comments,setComments]=useState(false)
+  const [sendButton,setSendButton]=useState(false)
+
+  const fetchData = async () => {
+    const res = await getPost();
+    if(res.status) setPost(res.data);
+  }
+
+  const fetchData2 = async () => {
+    const res = await getUser();
+    if (res.status) setUser(res.data);
+  }
+  
+
+  function showSendButton(){
+    (document.getElementById("comments") as HTMLInputElement).value!==""?setSendButton(true):setSendButton(false);
+  }
+
+  function searchForum(){
+    const value = (document.getElementById("search") as HTMLInputElement).value
+    value?setPost(post.filter((filter)=>filter.value.title==value)):setPost(post)
+    console.log(value)
+  }
+
+  useEffect(() => {
+    fetchData();
+    fetchData2();
+  },[]);
+  console.log(post);
+  // console.log(user);
 
   return (
-    <div className="border border-blue-300 px-16 pt-10">
+    <div className="px-16 pt-10">
 
-      <div className="text-2xl border border-red-300">
+      <div className="text-2xl">
         <span>Forum Diskusi</span>
       </div>
 
       <div className="mt-8">
 
-        <div className="flex border border-yellow-600">
+        <div className="flex">
 
           <div className="w-full">
-            <input className="w-full rounded p-2.5 shadow-lg bg-slate-100 text-black border border-gray-300" placeholder="Judul diskusi" spellCheck={false} />
+            <input className="w-full rounded p-2.5 shadow-lg bg-slate-100 text-black border border-gray-300" placeholder="Judul diskusi" id="search" spellCheck={false} />
           </div>
 
           <div className="ms-3">
-            <button className="h-full px-3 w-full bg-blue-800 text-white rounded transition duration-300 hover:bg-blue-950 shadow-lg">Search</button>
+            <button className="h-full px-3 w-full bg-blue-800 text-white rounded transition duration-300 hover:bg-blue-950 shadow-lg" onClick={()=>searchForum()}>Search</button>
           </div>
 
           <div>
-            <button className="h-full ms-5 bg-green-800 text-white rounded transition duration-300 py-2 px-4 hover:bg-blue-950 shadow-lg truncate">Diskusi Baru</button>
+            <Link to='/postForum'><button className="h-full ms-5 bg-green-800 text-white rounded transition duration-300 py-2 px-4 hover:bg-blue-950 shadow-lg truncate">Diskusi Baru</button></Link>
           </div>
         </div>
 
-        <div className="mt-6 flex flex-col gap-10 border border-green-500">
-          {[...Array(4)].map((item, index) => (
-            <div className="border border-gray-600 rounded-lg shadow-xl p-5">
-
-              <div>27.08.2022, 09.30 PM</div>
-              <div className="text-blue-600">Ahmad Na Jaemin</div>
-              <div className="mt-3">
-                Hi! Thanks for following our store, we have a special promo for you, please check terms and condition right here
+        <div className="mt-6 flex flex-col gap-10">
+          {post.map((post, index) => (
+            <div key={index} className="border border-gray-600 rounded-lg shadow-xl p-5">
+              <span className="text-blue-600">{user.filter((filter)=>filter.id==post.value.owner)[0].value.username}&nbsp;</span>
+              <span>{new Date(post.value.createdAt).toLocaleDateString()}</span>
+              <div className="text-2xl">{post.value.title}</div>
+              <div className="mt-3">{post.value.description}</div>
+              <button className="mt-3 ms-5 rounded-lg shadow-lg bg-blue-300 p-2 hover:bg-blue-400" onClick={()=>setComments(!comments)}>Comments</button>
+              {sendButton &&
+              <button className="mt-3 ms-5 rounded-lg shadow-lg bg-blue-300 p-2 hover:bg-blue-400">Send</button>
+              }
+              {comments&&
+              <div className="mt-3 ms-5">
+                <textarea className="w-full resize-none border-2 rounded-lg p-5" name="comments" id="comments" rows="3" placeholder="Join the discussion ..." onChange={()=>showSendButton()}></textarea>
               </div>
-
+              }
+              {post.value.comments.length>0 && 
+              <div className="mt-3 ms-5 flex flex-col gap-3">
+              {post.value.comments.map((comments,index)=>(
+                <div key={index} className="border border-gray-600 rounded-lg shadow-xl p-5">
+                <span className="text-blue-600">{user.filter((filter)=>filter.id==comments.owner)[0].value.username}&nbsp;</span>
+                <span>{new Date(comments.createdAt).toLocaleDateString()}</span>
+                <div className="mt-3">{comments.content}</div>
+              </div>
+              ))}
+              </div>
+              }
             </div>
           ))}
         </div>
