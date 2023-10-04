351ac2241772aaede90ae980e44f6c43e8dfa523
Rendy Pratama <126235207+Rendy752@users.noreply.github.com>
2023-10-04 23:58:35 +0700

    ADD: value text area checker, send button

--- frontend/src/pages/main/Forum.tsx
+++ frontend/src/pages/main/Forum.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import { getPost, getPostByOwner, getUser } from "../../api/services";
 import { Link } from 'react-router-dom';
+import { PlayIcon } from "@heroicons/react/24/solid";
+import { DocumentIcon } from "@heroicons/react/24/outline";
+
 
 const Forum = () => {
   const [post,setPost]=useState<any[]>([])
   const [user,setUser]=useState<any[]>([])
-  const [comments,setComments]=useState(false)
+  const [showComments,setShowComments]=useState(false)
+  const [comments,setComments]=useState("")
+  const [commentsLength,setCommentsLength]=useState(0)
   const [sendButton,setSendButton]=useState(false)
 
   const fetchData = async () => {
@@ -19,13 +24,15 @@ const Forum = () => {
   }
   
 
-  function showSendButton(){
-    (document.getElementById("comments") as HTMLInputElement).value!==""?setSendButton(true):setSendButton(false);
+  const showSendButton = event => {
+    setComments(event.target.value)
+    setCommentsLength(event.target.value.length)
+    event.target.value!==""?setSendButton(true):setSendButton(false)
   }
 
   function searchForum(){
     const value = (document.getElementById("search") as HTMLInputElement).value
-    value?setPost(post.filter((filter)=>filter.value.title==value)):setPost(post)
+    value?setPost(post.filter((filter)=>filter.value.title.includes(value))):setPost(post)
     console.log(value)
   }
 
@@ -34,7 +41,7 @@ const Forum = () => {
     fetchData2();
   },[]);
   console.log(post);
-  // console.log(user);
+  console.log(user);
 
   return (
     <div className="px-16 pt-10">
@@ -61,26 +68,28 @@ const Forum = () => {
         </div>
 
         <div className="mt-6 flex flex-col gap-10">
-          {post.map((post, index) => (
+          {post?.map((post, index) => (
             <div key={index} className="border border-gray-600 rounded-lg shadow-xl p-5">
-              <span className="text-blue-600">{user.filter((filter)=>filter.id==post.value.owner)[0].value.username}&nbsp;</span>
+              <span className="text-blue-600">{user?.filter((filter)=>filter.id==post?.value.owner)[0].value.username}&nbsp;</span>
               <span>{new Date(post.value.createdAt).toLocaleDateString()}</span>
               <div className="text-2xl">{post.value.title}</div>
               <div className="mt-3">{post.value.description}</div>
-              <button className="mt-3 ms-5 rounded-lg shadow-lg bg-blue-300 p-2 hover:bg-blue-400" onClick={()=>setComments(!comments)}>Comments</button>
-              {sendButton &&
-              <button className="mt-3 ms-5 rounded-lg shadow-lg bg-blue-300 p-2 hover:bg-blue-400">Send</button>
-              }
-              {comments&&
-              <div className="mt-3 ms-5">
-                <textarea className="w-full resize-none border-2 rounded-lg p-5" name="comments" id="comments" rows="3" placeholder="Join the discussion ..." onChange={()=>showSendButton()}></textarea>
+              <button className="mt-3 ms-5 rounded-lg shadow-lg bg-blue-300 p-2 hover:bg-blue-400" onClick={()=>setShowComments(!showComments)}>Comments</button>
+              {showComments&&
+              <div className="mt-3 ms-5 relative">
+                <textarea maxLength={400} className="w-full resize-none border-2 rounded-lg p-5" name="comments" id="comments" rows={4} value={comments} placeholder="Join the discussion ..." onChange={showSendButton}></textarea>
+                <button><PlayIcon className={`right-4 bottom-4 absolute w-6 ${sendButton ? "text-blue-600 hover:text-blue-900":" text-blue-200"}`} /></button>
+                <div className="absolute left-4 bottom-4 flex items-center">
+                  <span><DocumentIcon className="w-6 "/></span>
+                  <span>{commentsLength}/400</span>
+                </div>
               </div>
               }
               {post.value.comments.length>0 && 
               <div className="mt-3 ms-5 flex flex-col gap-3">
-              {post.value.comments.map((comments,index)=>(
+              {post?.value.comments.map((comments,index)=>(
                 <div key={index} className="border border-gray-600 rounded-lg shadow-xl p-5">
-                <span className="text-blue-600">{user.filter((filter)=>filter.id==comments.owner)[0].value.username}&nbsp;</span>
+                <span className="text-blue-600">{user?.filter((filter)=>filter.id==comments.owner)[0].value.username}&nbsp;</span>
                 <span>{new Date(comments.createdAt).toLocaleDateString()}</span>
                 <div className="mt-3">{comments.content}</div>
               </div>
