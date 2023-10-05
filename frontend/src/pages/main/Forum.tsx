99b71dd143fa4da0613a124a424c5f3e6d9a14a4
Rendy Pratama <126235207+Rendy752@users.noreply.github.com>
2023-10-05 20:00:04 +0700

    FIX: reset comment value every time switch comments for reply

--- frontend/src/pages/main/Forum.tsx
+++ frontend/src/pages/main/Forum.tsx
@@ -10,7 +10,7 @@ const Forum = () => {
   const [selectedPost,setSelectedPost]=useState<any[]>([])
   const [user,setUser]=useState<any[]>([])
   const [comments,setComments]=useState("")
-  const [popUpComment,setPopUpComment]=useState(-1)
+  const [inputComment,setInputComment]=useState(-1)
   const [search,setSearch]=useState("")
   const [commentsLength,setCommentsLength]=useState(0)
   const [sendButton,setSendButton]=useState(false)
@@ -51,28 +51,20 @@ const Forum = () => {
 
   return (
     <div className="px-16 pt-10">
-
       <div className="text-2xl">
         <span>Forum Diskusi</span>
       </div>
-
       <div className="mt-8">
-
         <div className="flex">
-
           <div className="w-full">
             <input className="w-full rounded p-2.5 shadow-lg bg-slate-100 text-black border border-gray-300" placeholder="Judul diskusi" value={search} onChange={searchForum} spellCheck={false} />
           </div>
-
-          <div className="ms-3">
-            <button className="h-full px-3 w-full bg-blue-800 text-white rounded transition duration-300 hover:bg-blue-950 shadow-lg">Search</button>
-          </div>
-
           <div>
             <Link to='/postForum'><button className="h-full ms-5 bg-green-800 text-white rounded transition duration-300 py-2 px-4 hover:bg-blue-950 shadow-lg truncate">Diskusi Baru</button></Link>
           </div>
         </div>
 
+        {/* post */}
         <div className="mt-6 flex flex-col gap-10">
           {selectedPost?.map((post, index) => (
             <div key={index} className="border border-gray-600 rounded-lg shadow-xl p-5">
@@ -80,17 +72,20 @@ const Forum = () => {
               <span>{new Date(post.value.createdAt).toLocaleDateString()}</span>
               <div className="text-2xl">{post.value.title}</div>
               <div className="mt-3">{post.value.description}</div>
-              <button className="mt-3 ms-5 rounded-lg shadow-lg bg-blue-300 p-2 hover:bg-blue-400" onClick={()=>setPopUpComment(index)}>Comments</button>
-              {index==popUpComment&&
+              <button className="mt-3 ms-5 rounded-lg shadow-lg bg-blue-300 p-2 hover:bg-blue-400" onClick={()=>{setInputComment(index);setComments("");setCommentsLength(0);setSendButton(false)}}>Comments</button>
+              {/* input comment */}
+              {index==inputComment&&
               <div className="mt-3 ms-5 relative">
                 <textarea maxLength={400} className="w-full resize-none border-2 rounded-lg p-5" rows={4} value={comments} placeholder="Join the discussion ..." onChange={showSendButton} spellCheck={false}></textarea>
                 <button><PlayIcon className={`right-4 bottom-4 absolute w-6 ${sendButton ? "text-blue-600 hover:text-blue-900":" text-blue-200"}`} /></button>
+                {/* comment length check */}
                 <div className="absolute left-4 bottom-4 flex items-center">
                   <span><DocumentIcon className="w-6 "/></span>
                   <span>{commentsLength}/400</span>
                 </div>
               </div>
               }
+              {/* list comment reply */}
               {post?.value.comments.length>0 && 
               <div className="mt-3 ms-5 flex flex-col gap-3">
               {post?.value.comments.map((comments,index)=>(
