57e7dabb7799a13a5a969d9c73393925eeb8d934
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-10-07 23:07:27 +0700

    ADD: some alat musik assets

--- frontend/src/utils/AlatMusikList.ts
+++ frontend/src/utils/AlatMusikList.ts
@@ -1,7 +1,12 @@
-import drum from "../assets/drum.jpg";
-import gitar from "../assets/gitar.jpg";
-import piano from "../assets/piano.jpg";
-import biola from "../assets/biola.png";
+import bass from "../assets/alatMusik/bass.jpg";
+import biola from "../assets/alatMusik/biola.png";
+import cello from "../assets/alatMusik/cello.jpg";
+import drum from "../assets/alatMusik/drum.jpg";
+import gitar from "../assets/alatMusik/gitar.jpg";
+import gitarListrik from "../assets/alatMusik/gitarListrik.jpg";
+import piano from "../assets/alatMusik/piano.jpg";
+import trompoet from "../assets/alatMusik/trompet.jpg";
+import ukulele from "../assets/alatMusik/ukulele.jpg";
 
 type TAlatMusik = {
   nama: string;
@@ -9,10 +14,15 @@ type TAlatMusik = {
 }
 
 const alatMusikList: TAlatMusik[] = [
-  { nama: 'drum', img: drum },
-  { nama: 'gitar', img: gitar },
-  { nama: 'piano', img: piano },
-  { nama: 'biola', img: biola },
+  {nama: 'bass', img: bass},
+  {nama: 'biola', img: biola},
+  {nama: 'cello', img: cello},
+  {nama: 'drum', img: drum},
+  {nama: 'gitar', img: gitar},
+  {nama: 'gitar listrik', img: gitarListrik},
+  {nama: 'piano', img: piano},
+  {nama: 'trompoet', img: trompoet},
+  {nama: 'ukulele', img: ukulele},
 ]
 
 export const getAlatMusikImg = (alatMusik: string) => {
