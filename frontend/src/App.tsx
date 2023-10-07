d808fd8d95d12eb17150e6c4145c55c898cba838
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-10-07 22:27:56 +0700

    ADD: not found page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import Setting from "./pages/main/Setting"
 import Profile from "./pages/dynamic/Profile"
 import ListMateri from "./pages/main/ListMateri"
 import Materi from "./pages/dynamic/Materi"
+import NotFound from "./components/_shared/NotFound"
 
 function App() {
   return (
@@ -29,7 +30,7 @@ function App() {
 
           <Route path='/resetpassword/:id' element={<ResetPassword />} />
 
-          <Route path='/*' element={<div>NOT FOUND</div>} />
+          <Route path='/*' element={<NotFound />} />
 
         </Routes>
       </div>
