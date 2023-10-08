47aca36da44253df99ca6840ddf691ab254649ac
usmanyap <usmanyap0301@gmail.com>
2023-10-08 21:56:55 +0700

    EDIT : Home

--- frontend/src/components/Home/MainSection.tsx
+++ frontend/src/components/Home/MainSection.tsx
@@ -1,93 +1,75 @@
 import TransitionIn from "../_shared/TransitionIn";
-import { motion, useTransform, useScroll } from "framer-motion";
-import { useRef } from "react";
-import pemainmusik1 from "../../assets/alatMusik/bass.jpg";
-import pemainmusik2 from "../../assets/pemain musik terkenal 1.jpeg";
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
 
-const MainSection = () => {
+import image1 from "../../assets/pemain musik terkenal 1.jpeg";
+import image2 from "../../assets/pemain musik terkenal 2.jpeg";
+import image3 from "../../assets/pemain musik terkenal 3.jpeg";
+
+const images = [image1, image2, image3];
+
+const MainSection: React.FC = () => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const [percentage, setPercentage] = useState(0);
+  const [mouseDownX, setMouseDownX] = useState(0);
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    setMouseDownX(e.clientX);
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    const mouseDelta = mouseDownX - e.clientX;
+    const maxDelta = window.innerWidth / 2;
+    const nextPercentageUnconstrained = (mouseDelta / maxDelta) * -100;
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+    setPercentage(nextPercentage);
+
+    if (targetRef.current) {
+      targetRef.current.style.transform = `translate(${nextPercentage}%, -50%)`;
+    }
+  };
+
+  const handleMouseUp = () => {
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+  };
+
+  const handleButtonClick = () => {
+    const newPercentage = percentage === 0 ? -50 : 0;
+
+    setPercentage(newPercentage);
+
+    if (targetRef.current) {
+      targetRef.current.style.transition = "transform 0.5s ease";
+      targetRef.current.style.transform = `translate(${newPercentage}%, -50%)`;
+    }
+  };
+
   return (
-    <div>
+    <div className="h-screen overflow-hidden relative">
       <TransitionIn from="bottom">
-        <div className="bg-neutral-800">
-          <div className="flex h-48 items-center justify-center">
-            <span className="font-semibold uppercase text-neutral-500">Scroll down</span>
-          </div>
-          <HorizontalScrollCarousel />
-          <div className="flex h-48 items-center justify-center">
-            <span className="font-semibold uppercase text-neutral-500">Scroll up</span>
-          </div>
+        <div id="image-track" ref={targetRef} className="flex absolute top-1/2 translate-y-1/2 gap-4" onMouseDown={handleMouseDown}>
+          {images.map((src, index) => (
+            <motion.img
+              key={index}
+              className="image"
+              src={src}
+              alt={`Image ${index}`}
+              draggable={false}
+              style={{
+                objectPosition: `${100 + percentage}% center`,
+                width: "30%",
+                height: "300px",
+              }}
+            />
+          ))}
         </div>
       </TransitionIn>
     </div>
   );
 };
 
-const HorizontalScrollCarousel = () => {
-  const targetRef = useRef<HTMLDivElement | null>(null);
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-  });
-
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
-
-  return (
-    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
-      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
-        <motion.div style={{ x }} className="flex gap-4">
-          {cards.map((card) => {
-            return <Card card={card} key={card.id} />;
-          })}
-        </motion.div>
-      </div>
-    </section>
-  );
-};
-
-const Card = ({ card }: { card: CardType }) => {
-  return (
-    <div key={card.id} className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200">
-      <div
-        style={{
-          backgroundImage: `url(${card.url})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-        className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
-      ></div>
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center">
-        <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">{card.title}</p>
-        <p className="bg-gradient-to-br from-white/20 to-white/0 p-4 text-lg font-normal text-white backdrop-blur-lg">{card.desc}</p>
-      </div>
-    </div>
-  );
-};
-
 export default MainSection;
-
-type CardType = {
-  url: string;
-  title: string;
-  desc: string;
-  id: number;
-};
-
-const cards: CardType[] = [
-  {
-    url: pemainmusik1,
-    title: "Title 1",
-    desc: "asdasd",
-    id: 1,
-  },
-  {
-    url: pemainmusik2,
-    title: "Title 1",
-    desc: "asdasd",
-    id: 2,
-  },
-  {
-    url: pemainmusik1,
-    title: "Title 1",
-    desc: "asdasd",
-    id: 3,
-  },
-];
