e8d55be4a5bc19497b8360b17c949afdefc2caa3
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-10-10 22:47:50 +0700

    ADD: backend cors whitelist to dev

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import { validateToken } from "./config/validation.js";
 const app = express();
 
 const corsOptions = {
-  origin: "https://maribermusik.fanesp.online",
+  origin: ["https://maribermusik.fanesp.online", "http://localhost:5173"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
 
