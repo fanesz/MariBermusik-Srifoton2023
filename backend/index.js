33b06d024a283025ad7cfc6216850c686c937482
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-10-10 17:46:22 +0700

    SERVER: add CORS whitelist

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,20 @@ import forumRoute from "./routes/forumRoute.js";
 
 const app = express();
 
+const corsOptions = {
+  origin: 'https://maribermusik.fanesp.online/',
+  optionsSuccessStatus: 200
+}
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/user', userRoute);
-app.use('/api/materi', materiRoute);
-app.use('/api/login', loginRoute);
-app.use('/api/forgetPass', forgetPassRoute);
-app.use('/api/forum', forumRoute);
+app.use('/api/user', cors(corsOptions), userRoute);
+app.use('/api/materi', cors(corsOptions), materiRoute);
+app.use('/api/login', cors(corsOptions), loginRoute);
+app.use('/api/forgetPass', cors(corsOptions), forgetPassRoute);
+app.use('/api/forum', cors(corsOptions), forumRoute);
 
 
 app.listen(5000, () => console.log('Server running at port 5000'));
